c1509767a89fb7e1081f79a2de517ad86836fb6c
Shaibu <s.shaibu.jnr@gmail.com>
2016-12-07 10:37:13 +0100

    cod refactor for route http get methods

--- web/helper.js
+++ web/helper.js
@@ -5,5 +5,51 @@ class Helper{
     static getRootCourse(){return "/courses";}
     static getRootQuestion(){return "/questions";}
     static getRoots(){return ["/faculties","/departments","/courses","/questions"];}
+    static toSelectQuery(columns,table,query){
+        if(Array.isArray(columns))var columns = columns.join(" , ");
+        var props = this.getListOfObjectProps(query);
+        var qs = "";
+        if(this.isObjectEmpty(query)) qs = "SELECT "+columns+" FROM "+table.name;
+        else qs = "SELECT "+columns+" FROM "+table.name+" WHERE ";
+        var sqslist = [];
+        for(var i=0;i<props.length;i++){
+            var sqs = String(props[i]+" = "+"\""+query[props[i]]+"\"");
+            sqslist.push(sqs);
+        }
+        qs += sqslist.join(" AND ");
+        qs+= ";";
+        console.log(qs);
+        return qs;
+
+    }
+
+    static  numberOfObjectProps(obj){
+        var attr_count = 0;
+        for(var prop in obj){
+            if(obj.hasOwnProperty(prop)){
+                attr_count++;
+            }
+        }
+        return attr_count;
+
+    }
+    static getListOfObjectProps(obj){
+        var props = [];
+        for(var prop in obj){
+            if(obj.hasOwnProperty(prop)){
+                props.push(prop);
+            }
+        }
+        return props; 
+    }
+    static  isObjectEmpty(obj){
+        for(var prop in obj){
+            if(obj.hasOwnProperty(prop)){
+               return false;
+
+            }
+        }
+        return true;
+    }
 }
 module.exports = Helper;
--- web/router.js
+++ web/router.js
@@ -14,37 +14,70 @@ var dt = dbh.getDepartmentTable();
 var ct = dbh.getCourseTable();
 var qt = dbh.getQuestionTable(); 
 
-function isObjectEmpty(obj){
-    for(var prop in obj){
-        if(obj.hasOwnProperty(prop)){
-            return false;
 
-        }
-    }
-    return true;
-}
 
 function route(url,res){
     var path = purl.parse(url).pathname;
     var query = querystring.parse(purl.parse(url).query);
-    if(path === "/" && isObjectEmpty(query)){
+    if(path === "/" && Helper.isObjectEmpty(query)){
         res.write(JSON.stringify(Helper.getRoots()));
         res.end();
     }
-    else if(path === Helper.getRootFaculty() && isObjectEmpty(query)){
-        displayFaculties(res);
+    else if(path === Helper.getRootFaculty()){
+        var faculties = [];
+        var qs = Helper.toSelectQuery("*",ft,query);
+        db.each(qs,
+        function(err,row){
+            var nFaculty = new Faculty(row.name,row.xid);
+            faculties.push(nFaculty);
+
+        }
+        ,function(err,val){
+            res.write(JSON.stringify(faculties));
+            res.end();
+
+        });
     }
-    else if(path === Helper.getRootFaculty() && !isObjectEmpty(query)){
-        displayFacultiesbyOptions(query,res);
-    }
-    else if(path === Helper.getRootDepartment() && isObjectEmpty(query)){
-        displayDepartments(res);
+    else if(path === Helper.getRootDepartment()){
+        var departments = [];
+        var qs = Helper.toSelectQuery("*",dt,query);
+        db.each(qs,
+        function(err,row){
+            var nDepartment = new Department(row.name,row.xid,row.fxid);
+            departments.push(nDepartment);
+        },
+        function(err,val){
+            res.write(JSON.stringify(departments));
+            res.end();
+        });
     }
     else if(path === Helper.getRootCourse()){
-        displayCourses(res);
+        var courses = [];
+        var qs = Helper.toSelectQuery("*",ct,query);
+        db.each(qs,
+        function(err,row){
+            var nCourse = new Course(row.title,row.code,row.xid,row.dxids);
+            courses.push(nCourse);
+        },
+        function(err,val){
+            res.write(JSON.stringify(courses));
+            res.end();
+        });
     }
     else if(path === Helper.getRootQuestion()){
-        displayQuestions(res);
+        var questions = [];
+        var qs = Helper.toSelectQuery("*",qt,query);
+        db.each(qs,
+    function(err,row){
+        var nQuestion = new Question(row.title,row.answer,row.xid,row.cxid);
+        questions.push(nQuestion);
+
+    },
+    function(err,val){
+        res.write(JSON.stringify(questions));
+        res.end();
+
+    });
     }
     else{
         res.write("path not specified");
@@ -55,118 +88,13 @@ function route(url,res){
 
 }
 
-function displayFacultiesbyOptions(obj,response){
-    var qname = obj.name;
-    var qxid = obj.xid;
-    console.log(qname+" "+qxid);
-    if(qname && qxid){
-        //display by name and xid
-        var faculties = [];
-        db.each("SELECT * FROM "+ft.name+" WHERE name= \""+qname+"\" AND xid= \""+qxid+"\";",
-        function(err,row){
-            var nFaculty = new Faculty(row.name,row.xid);
-            faculties.push(nFaculty);
 
-        },
-        function(err,val){
-            response.write(JSON.stringify(faculties));
-            response.end();
 
-        });
-    }
-    else if(qname && !qxid){
-        //display by name
-        var faculties = [];
-        db.each("SELECT * FROM "+ft.name+" WHERE name=\""+qname+"\";",
-        function(err,row){
-            var nFaculty = new Faculty(row.name,row.xid);
-            faculties.push(nFaculty);
 
-        },
-        function(err,val){
-            response.write(JSON.stringify(faculties));
-            response.end();
 
-        });
 
-    }
-    else if(!qname && qxid){
-        //display by xid
-        var faculties = [];
-        db.each("SELECT * FROM "+ft.name+" WHERE xid=\""+qxid+"\";",
-        function(err,row){
-            var nFaculty = new Faculty(row.name,row.xid);
-            faculties.push(nFaculty);
 
-        },
-        function(err,val){
-            response.write(JSON.stringify(faculties));
-            response.end();
 
-        });
-    }
-    else{
-        response.write("invalid request");
-        response.end();
-    }
-}
 
-function displayFaculties(response){
-    var faculties = [];
-    db.each("SELECT * FROM "+ft.name+";",function(err,row){
-        var nFaculty = new Faculty(row.name,row.xid);
-        faculties.push(nFaculty);
-            
-    },function(err,val){
-        response.write(JSON.stringify(faculties));
-        response.end();
-     });
-}
-
-function displayDepartments(response){
-    var departments = [];
-    db.each("SELECT * FROM "+dt.name+";",
-    function(err,row){
-        var nDepartment = new Department(row.name,row.xid,row.fxid);
-        departments.push(nDepartment);
-    }
-    ,function(err,val){
-        response.write(JSON.stringify(departments));
-        response.end();
-    });
-    
-    
-}
-
-function displayCourses(response){
-    var courses = [];
-    db.each("SELECT * FROM "+ct.name+";",
-    function(err,row){
-        var nCourse = new Course(row.title,row.code,row.xid,row.dxid);
-        courses.push(nCourse);
-
-    },
-    function(err,val){
-        response.write(JSON.stringify(courses));
-        response.end();
-
-    });
-    
-}
-
-function displayQuestions(response){
-    var questions = [];
-    db.each("SELECT * FROM "+qt.name+";",
-    function(err,row){
-        var nQuestion = new Question(row.title,row.answer,row.xid,row.cxid);
-        questions.push(nQuestion);
-
-    },
-    function(err,val){
-        response.write(JSON.stringify(questions));
-        response.end();
-
-    });
-   
-}
 module.exports = route;
+
